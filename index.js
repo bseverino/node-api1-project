d474ae19bf017f72a273ae57617b8d80ca4518da
B Severino <biancasev@gmail.com>
2020-01-13 16:14:21 -0500

    Implemented insert function

--- index.js
+++ index.js
@@ -4,28 +4,45 @@ const Hubs = require('./data/db.js')
 const server = express()
 server.use(express.json())
 
-server.get('/api/hubs', (req, res) => {
+server.get('/api/users', (req, res) => {
     Hubs.find()
-        .then(hubs => {
-            res.status(200).json(hubs)
+        .then(users => {
+            res.status(200).json(users)
         })
         .catch(err => {
             console.log(err)
-            err.status(500).json({ errorMessage: 'Sorry, we ran into an error getting the list of hubs' })
+            err.status(500).json({ errorMessage: 'Sorry, we ran into an error getting the list of users.' })
         })
 })
 
-server.get('/api/hubs/:id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
     const id = req.params.id
 
     Hubs.findById(id)
-        .then(hub => {
-            res.status(200).json(hub)
+        .then(user => {
+            res.status(200).json(user)
         })
         .catch(err => {
             console.log(err)
-            res.status(500).json({ errorMessage: 'Sorry, we ran into an error getting that hubs' })
+            res.status(500).json({ errorMessage: 'Sorry, we ran into an error getting that user.' })
         })
+})
+
+server.post('/api/users', (req, res) => {
+    const userData = req.body
+
+    if (userData['name'] && userData['bio']) {
+        Hubs.insert(userData)
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({ errorMessage: 'Sorry, we ran into an error creating the user.' })
+            })
+    } else {
+        res.status(500).json({ errorMessage: 'Please provide name and bio for the user.' })
+    }
 })
 
 const port = 8000
